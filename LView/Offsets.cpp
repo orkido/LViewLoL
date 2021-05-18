4e163002b3afca8e975edf4930102621fa6d0bc5
Le Quyet <30817207+Jo3yVX@users.noreply.github.com>
2021-05-18 16:24:14 +0700

    Update offsets for v11.10.

--- LView/Offsets.cpp
+++ LView/Offsets.cpp
@@ -1,66 +1,67 @@
 #include "Offsets.h"
 
 Offsets::Offsets() {};
+//Credit to vegacold1995 from unknowncheats.
+int Offsets::GameTime = 0x2FADB80; //		11.9 ==> 11.10
 
-int Offsets::GameTime = 0x2FB0754;
-int Offsets::MinimapObject = 0x2FB81EC;
-int Offsets::ViewProjMatrices = 0x02FDBBB0;
-int Offsets::Renderer = 0x2FDEA1C;
-int Offsets::ObjectManager = 0x171BBC4;
-int Offsets::LocalPlayer = 0x2FB81E8;
-int Offsets::UnderMouseObject = 0x2FACDA0;
-int Offsets::ObjItemList = 0x37F4;
-int Offsets::ObjSpellBook = 0x2C60;
-int Offsets::ObjName = 0x304C;
-int Offsets::ObjLvl = 0x37BC;
+int Offsets::ObjIndex = 0x20; //			UNCHANGED
+int Offsets::ObjTeam = 0x4C; //			UNCHANGED
+int Offsets::ObjNetworkID = 0xCC; //			UNCHANGED
+int Offsets::ObjPos = 0x1d8; //			UNCHANGED
+int Offsets::ObjVisibility = 0x270; //			UNCHANGED
+int Offsets::ObjSpawnCount = 0x284; //			UNCHANGED
+int Offsets::ObjSrcIndex = 0x290; //			UNCHANGED
+int Offsets::ObjMana = 0x298; //			UNCHANGED
+int Offsets::ObjHealth = 0xD98; //			UNCHANGED
+int Offsets::ObjMaxHealth = 0xDA8; //			UNCHANGED
+int Offsets::ObjArmor = 0x12C8; //		11.9 ==> 11.10
+int Offsets::ObjMagicRes = 0x12D0; //		11.9 ==> 11.10
+int Offsets::ObjBaseAtk = 0x12A0; //		11.9 ==> 11.10
+int Offsets::ObjBonusAtk = 0x1218; //		UNCHANGED
+int Offsets::ObjMoveSpeed = 0x12E0; //		11.9 ==> 11.10
+int Offsets::ObjSpellBook = 0x2880; //		11.9 ==> 11.10
+int Offsets::ObjName = 0x2C6C; //		11.9 ==> 11.10
+int Offsets::ObjLvl = 0x33DC; //		11.9 ==> 11.10
+int Offsets::ObjExpiry = 0x298; //			UNCHANGED
+int Offsets::ObjCrit = 0x12C4; //		11.9 ==> 11.10
+int Offsets::ObjCritMulti = 0x12B4; //		11.9 ==> 11.10
+int Offsets::ObjAbilityPower = 0x1228; //		UNCHANGED
+int Offsets::ObjAtkSpeedMulti = 0x129C; //		11.9 ==> 11.10
+int Offsets::ObjItemList = 0x3414; //		NOT SURE
 
-int Offsets::RendererWidth = 0xC;
-int Offsets::RendererHeight = 0x10;
+int Offsets::ItemListItem = 0xC; //			NOT TOUCHED
+int Offsets::ItemInfo = 0x20; //			NOT TOUCHED
+int Offsets::ItemInfoId = 0x68; //			NOT TOUCHED
 
-int Offsets::ObjIndex = 0x20;
-int Offsets::ObjTeam = 0x4C;
-int Offsets::ObjNetworkID = 0xCC;
-int Offsets::ObjPos = 0x1d8;
-int Offsets::ObjVisibility = 0x270;
-int Offsets::ObjSpawnCount = 0x284;
-int Offsets::ObjSrcIndex = 0x290;
-int Offsets::ObjMana = 0x298;
-int Offsets::ObjHealth = 0xD98;
-int Offsets::ObjMaxHealth = 0xDA8;
-int Offsets::ObjArmor = 0x12C4;
-int Offsets::ObjMagicRes = 0x12CC;
-int Offsets::ObjBaseAtk = 0x129C;
-int Offsets::ObjBonusAtk = 0x1218;
-int Offsets::ObjMoveSpeed = 0x12DC;
-int Offsets::ObjExpiry = 0x298;
-int Offsets::ObjCrit = 0x12C0;
-int Offsets::ObjCritMulti = 0x12B0;
-int Offsets::ObjAbilityPower = 0x1228;
-int Offsets::ObjAtkSpeedMulti = 0x1270;
+int Offsets::ViewProjMatrices = 0x2FD92A8; //		11.9 ==> 11.10
+int Offsets::Renderer = 0x2FDC11C; //		11.9 ==> 11.10 
+int Offsets::RendererWidth = 0x0C; //			UNCHANGED
+int Offsets::RendererHeight = 0x10; //			UNCHANGED
 
-int Offsets::ItemListItem = 0xC;
-int Offsets::ItemInfo = 0x20;
-int Offsets::ItemInfoId = 0x68;
+int Offsets::SpellSlotLevel = 0x20; //			UNCHANGED
+int Offsets::SpellSlotTime = 0x28; //			UNCHANGED
+int Offsets::SpellSlotDamage = 0x94; //			UNCHANGED
+int Offsets::SpellSlotSpellInfo = 0x13C; //			UNCHANGED
+int Offsets::SpellInfoSpellData = 0x44; //			UNCHANGED
+int Offsets::SpellDataSpellName = 0x6C; //			11.9 ==> 11.10
+int Offsets::SpellDataMissileName = 0x6C; //			11.9 ==> 11.10
 
-int Offsets::SpellSlotLevel = 0x20;
-int Offsets::SpellSlotTime = 0x28;
-int Offsets::SpellSlotDamage = 0x94;
-int Offsets::SpellSlotSpellInfo = 0x13C;
-int Offsets::SpellInfoSpellData = 0x44;
-int Offsets::SpellDataSpellName = 0x64;
-int Offsets::SpellDataMissileName = 0x64;
+int Offsets::ObjectManager = 0x1719438; //		11.9 ==> 11.10
+int Offsets::LocalPlayer = 0x2FB5814; //		11.9 ==> 11.10
+int Offsets::UnderMouseObject = 0x2FAA1F8; //		11.9 ==> 11.10
+int Offsets::ObjectMapCount = 0x2C; //			UNCHANGED
+int Offsets::ObjectMapRoot = 0x28; //			UNCHANGED
+int Offsets::ObjectMapNodeNetId = 0x10; //			UNCHANGED
+int Offsets::ObjectMapNodeObject = 0x14; //			UNCHANGED
 
-int Offsets::MissileSpellInfo = 0x258;
-int Offsets::MissileSrcIdx = 0x2B8;
-int Offsets::MissileDestIdx = 0x310;
-int Offsets::MissileStartPos = 0x2D0;
-int Offsets::MissileEndPos = 0x2DC;
+int Offsets::MissileSpellInfo = 0x258; //			UNCHANGED
+int Offsets::MissileSrcIdx = 0x2B8; //			UNCHANGED
+int Offsets::MissileDestIdx = 0x310; //			UNCHANGED
+int Offsets::MissileStartPos = 0x2D0; //			UNCHANGED
+int Offsets::MissileEndPos = 0x2DC; //			UNCHANGED
 
-int Offsets::ObjectMapCount = 0x2C;
-int Offsets::ObjectMapRoot = 0x28;
-int Offsets::ObjectMapNodeNetId = 0x10;
-int Offsets::ObjectMapNodeObject = 0x14;
+int Offsets::MinimapObject = 0x2FB5810; //		11.9 ==> 11.10
 
-int Offsets::MinimapObjectHud = 0x88;
-int Offsets::MinimapHudPos = 0x60;
-int Offsets::MinimapHudSize = 0x68;
+int Offsets::MinimapObjectHud = 0x88; //			UNCHANGED
+int Offsets::MinimapHudPos = 0x60; //			UNCHANGED
+int Offsets::MinimapHudSize = 0x68; //			UNCHANGED
